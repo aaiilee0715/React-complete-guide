1ea6ae5dc4cefca913cc3965806e646ca2d74449
aaiilee0715 <achansylee@outlook.com>
2022-05-24 13:24:21 -0700

    Updated the state name changed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,29 @@
+import React, { useState } from 'react'
+
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  //function clickHandler(){}
+  const [title, setTitle] = useState(props.title) //must be called inside component function
+  //First state is the value, section state is setting a new state
+
+  // let title = props.title
+
+  const clickHandler = () => {
+    setTitle('Updated!')
+    console.log(title)
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
