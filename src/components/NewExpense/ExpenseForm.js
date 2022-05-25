aedd17386d6c994557d804ee9398927239d66e03
aaiilee0715 <achansylee@outlook.com>
2022-05-25 16:57:23 -0700

    Added event and state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //Can have more then 1 state set per component
   const [enteredTitle, setEnteredTitle] = useState('') //sate variable
   const [enteredAmount, setEnteredAmount] = useState('')
@@ -59,8 +59,8 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     }
-    console.log(expenseData)
 
+    props.onSaveExpenseData(expenseData)
     //two-way binding
     setEnteredTitle('')
     setEnteredAmount('')
