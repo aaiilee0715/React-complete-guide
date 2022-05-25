a3c7e628293ea2d701e3b3985ae8228cc26793a8
aaiilee0715 <achansylee@outlook.com>
2022-05-25 11:40:52 -0700

    Added new expense files and styling

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+//this is a form where user can input their expense data
+
+import React from 'react'
+import './NewExpense.css'
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  )
+}
+
+export default NewExpense
