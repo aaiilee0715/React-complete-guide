8cbf228c9940b33cfc2438c63d3c1dde9a56cb67
aaiilee0715 <achansylee@outlook.com>
2022-05-25 17:47:18 -0700

    Converted to presentational component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,24 +9,25 @@ const ExpenseItem = (props) => {
 
   //must be called inside component function
   //First state is the value, section state is setting a new state
-  const [title, setTitle] = useState(props.title)
+  // const [title, setTitle] = useState(props.title)
   console.log('ExpenseItem evaluated by React')
 
   // let title = props.title
 
-  const clickHandler = () => {
-    setTitle('Updated!')
-    console.log(title)
-  }
+  // const clickHandler = () => {
+  //   setTitle('Updated!')
+  //   console.log(title)
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        {/* <h2>{title}</h2> */}
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   )
 }
