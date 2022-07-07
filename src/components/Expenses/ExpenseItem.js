8334d7b13eaf0c07a67a770471a6eed76cc11a4c
aaiilee0715 <61571099+aaiilee0715@users.noreply.github.com>
2022-07-07 15:52:21 -0700

    Added a lists

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import Card from '../UI/Card'
-import ExpenseDate from './ExpenseDate'
-import './ExpenseItem.css'
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
+//function clickHandler(){}
+
+//must be called inside component function
+//First state is the value, section state is setting a new state
+// const [title, setTitle] = useState(props.title)
+// console.log('ExpenseItem evaluated by React')
+
+// let title = props.title
+
+// const clickHandler = () => {
+//   setTitle('Updated!')
+//   console.log(title)
+// }
 const ExpenseItem = (props) => {
-  //function clickHandler(){}
-
-  //must be called inside component function
-  //First state is the value, section state is setting a new state
-  // const [title, setTitle] = useState(props.title)
-  console.log('ExpenseItem evaluated by React')
-
-  // let title = props.title
-
-  // const clickHandler = () => {
-  //   setTitle('Updated!')
-  //   console.log(title)
-  // }
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        {/* <h2>{title}</h2> */}
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
-    </Card>
-  )
-}
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          {/* <h2>{title}</h2> */}
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
+      </Card>
+    </li>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
